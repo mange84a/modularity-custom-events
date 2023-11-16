e4c335497b6d0bb29581c45ef7813b81d5c6ecfb
Magnus <kontakt@magnus-andersson.nu>
2023-11-16 13:08:31 +0100

    update

---
+++ source/php/AcfFields/php/customevents-settings.php
@@ -0,0 +1,68 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_6552415451a18',
+    'title' => __('Display custom events', 'modularity-customevents'),
+    'fields' => array(
+        0 => array(
+            'key' => 'field_655241543010c',
+            'label' => __('Kategori', 'modularity-customevents'),
+            'name' => 'event_categories',
+            'aria-label' => '',
+            'type' => 'taxonomy',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'taxonomy' => 'custom-events_category',
+            'add_term' => 0,
+            'save_terms' => 0,
+            'load_terms' => 0,
+            'return_format' => 'id',
+            'field_type' => 'checkbox',
+            'acfe_bidirectional' => array(
+                'acfe_bidirectional_enabled' => '0',
+            ),
+            'multiple' => 0,
+            'allow_null' => 0,
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'block',
+                'operator' => '==',
+                'value' => 'acf/customevents',
+            ),
+        ),
+        1 => array(
+            0 => array(
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'mod-customevents',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'left',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+    'show_in_rest' => 0,
+    'acfe_display_title' => 'Display custom events',
+    'acfe_autosync' => array(
+        0 => 'json',
+    ),
+    'acfe_form' => 0,
+    'acfe_meta' => '',
+    'acfe_note' => '',
+));
+}
